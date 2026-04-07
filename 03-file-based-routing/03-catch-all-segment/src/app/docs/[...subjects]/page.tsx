362fdded2b8d991ee24ca38120e295aadc3af4f0
heeyeon <asd95121@gmail.com>
2026-04-07 11:07:48 +0900

    learn. 포괄적 세그먼트 part.2 - 브래드 크럼(Bread curmbs)

---
+++ 03-file-based-routing/03-catch-all-segment/src/app/docs/[...subjects]/page.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link'
+import { LucideHome, LucideChevronRight } from 'lucide-react'
+import { cn } from '@/utils'
+import { Fragment } from 'react/jsx-runtime'
+
+
+export default async function FundamentalPage({ params }: PageProps<'/docs/[...subjects]'>) {
+
+  // subjects = URL에 인코딩된 한글 경로를 포함하는 배열
+  const { subjects } = await params 
+
+  // decodedSubjects = 디코딩한 한글 경로를 포함하는 배열
+  const decodedSubjects = subjects.map((subject) => decodeURIComponent(subject))
+  // console.log(decodedSubjects)
+
+  // 현재 페이지의 제목
+  const lastSubjectTitle = decodedSubjects.at(-1)?.replace(/-/g, ' ')
+
+  return (
+    <section className="w-full space-y-12">
+      {/* 상대 경로 안내 (Breadcrumbs) */}
+      <nav className="flex h-10 items-center gap-x-1 font-bold text-slate-400">
+        <Link
+          className={cn(
+            'flex shrink-0 items-center gap-x-1.5',
+             'transition-colors hover:text-blue-600',
+          )}
+          href='/docs/기초'
+        >
+          <LucideHome className="size-4" /> 학습 문서 홈
+        </Link>
+        {
+          decodedSubjects.map((subject, index)=>{
+            const isLast = index === decodedSubjects.length - 1 
+            const href = `/docs/${subjects.slice(0, index + 1).join('/')}`
+
+            return(
+              <Fragment key={index}>
+                <LucideChevronRight className='size-3.5 shrink-0 text-slate-300' />
+                <Link href={href} className={cn(
+                  'px-4 py-1 border rounded-full',
+                  isLast ? 
+                  'border-blue-600 bg-blue-600 text-white shadow-md'
+                  : 'border-transparent hover:bg-blue-50 hover:text-blue-500'
+                )}>{subject.replace(/-/g, ' ')}</Link>
+              </Fragment>
+            )
+          })
+        }
+      </nav>
+
+      <h1 className='text-4xl font-black'>{lastSubjectTitle}</h1>
+    </section>
+  )
+}
