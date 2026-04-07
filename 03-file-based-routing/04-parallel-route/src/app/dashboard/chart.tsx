e582b01fdfcea1bb1c779774ef778b129f63979c
heeyeon <asd95121@gmail.com>
2026-04-07 13:29:24 +0900

    learn. 대시보드 레이아웃 구성
    - 페이지 view
    - 그래프 차트 view
    - 사용자 통계 view

---
+++ 03-file-based-routing/04-parallel-route/src/app/dashboard/chart.tsx
@@ -0,0 +1,7 @@
+export default function Chart(){
+  return(
+    <div className="h-100 p-5 bg-rose-200">
+      <h2 className="text-4xl font-bold">그래프 차트</h2>
+    </div>
+  )
+}
---
+++ 03-file-based-routing/04-parallel-route/src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <section className="bg-stone-100 self-stretch p-2 h-100">
+      <h1 className="text-4xl font-black">페이지</h1>
+    </section>
+  )
+}
